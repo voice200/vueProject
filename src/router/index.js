237a8cc65b512f83dd5c0ce7a4d98fba0b270fc9
voice200 <voice200rabota@gmail.com>
2020-11-12 03:34:26 +0200

    add files

---
+++ src/router/index.js
@@ -0,0 +1,61 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from "@/views/auth/Login";
+import Registration from "@/views/auth/Registration";
+import Ad from "@/views/ads/Ad";
+import NewAd from "@/views/ads/NewAd";
+import AdList from "@/views/ads/AdList";
+import Orders from "@/views/user/Orders";
+import Home from "@/views/Home";
+import NotFoundComponent from "@/views/NotFoundComponent";
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/registration',
+    name: 'registration',
+    component: Registration
+  },
+  {
+    path: '/ad/:id',
+    name: 'ad',
+    component: Ad
+  },
+  {
+    path: '/new',
+    name: 'new',
+    component: NewAd
+  },
+  {
+    path: '/orders',
+    name: 'orders',
+    component: Orders
+  },
+  {
+    path: '/list',
+    name: 'list',
+    component: AdList
+  },
+  {
+    path: '*',
+    component: NotFoundComponent
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router
